75dd58b70d685f5d32640f55d1f07b4bf8bbffeb
Jayden Li <126529751+JaydenLi1@users.noreply.github.com>
2023-04-26 17:57:00 -0500

    Add files via upload

---
+++ Project101/rolladice.py
@@ -0,0 +1,44 @@
+import random
+print('Do you want to roll a dice?')
+response = "y"
+
+while response == "y":
+    num = random.randint(1, 6)
+    if num == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    elif num == 2:
+        print("[-----]")
+        print("[0    ]")
+        print("[     ]")
+        print("[    0]")
+        print("[-----]")
+    elif num == 3:
+        print("[-----]")
+        print("[0    ]")
+        print("[  0  ]")
+        print("[    0]")
+        print("[-----]")
+    elif num == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    elif num == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    elif num == 6:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+    response = input('Press y to keep playing, press n to exit.')
+print(response)
